2a1f87eb5ebe730ebd1054d0084435801d78ac25
Andrew Njoo <adnjoo@gmail.com>
2024-06-16 10:16:04 -0700

    switch

--- app/(landing)/LandingBody.tsx
+++ app/(landing)/LandingBody.tsx
@@ -5,15 +5,17 @@ import { Loader2 } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
 import { getThumbnail } from "@/lib/utils";
 import { Example } from "./page";
 
-export default function LandingBody({ examples}: { examples: Example[]}) {
+export default function LandingBody({ examples }: { examples: Example[] }) {
   const [url, setUrl] = useState("");
   const [summary, setSummary] = useState("");
   const [thumbnailUrl, setThumbnailUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [hoveredExample, setHoveredExample] = useState<Example | null>(null);
+  const [showExamples, setShowExamples] = useState(true);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newUrl = event.target.value;
@@ -92,29 +94,36 @@ export default function LandingBody({ examples}: { examples: Example[]}) {
 
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <div className="flex flex-col mb-8 mx-auto">
-        <span className="text-center text-lg font-semibold">Examples</span>
-        <div className="animate-in grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8 mx-auto">
-          {examples.map((example: Example) => (
-            <div key={example.url} className="relative cursor-pointer"
-              onMouseEnter={() => handleMouseEnter(example)}
-              onMouseLeave={handleMouseLeave}
-              onClick={() => handleThumbnailClick(example.url)}
-            >
-              <img
-                className='max-w-[120px] sm:max-w-[180px] rounded-sm shadow-md cursor-pointer z-50'
-                src={example.thumbnail}
-                alt="thumbnail"
-              />
-              <div
-                className={`absolute transition bottom-0 left-0 w-full bg-black bg-opacity-75 text-white text-center text-xs p-1 z-0 ${example === hoveredExample ? 'opacity-100' : 'opacity-0'}`}
-              >
-                {example.title}
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="flex items-center mb-8 mx-auto">
+        <span className="mr-2 text-lg font-semibold">Show Examples</span>
+        <Switch checked={showExamples} onCheckedChange={setShowExamples} />
       </div>
+
+      {showExamples && (
+        <div className="flex flex-col mb-8 mx-auto">
+          <span className="text-center text-lg font-semibold">Examples</span>
+          <div className="animate-in grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8 mx-auto">
+            {examples.map((example: Example) => (
+              <div key={example.url} className="relative cursor-pointer"
+                onMouseEnter={() => handleMouseEnter(example)}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => handleThumbnailClick(example.url)}
+              >
+                <img
+                  className='max-w-[120px] sm:max-w-[180px] rounded-sm shadow-md cursor-pointer z-50'
+                  src={example.thumbnail}
+                  alt="thumbnail"
+                />
+                <div
+                  className={`absolute transition bottom-0 left-0 w-full bg-black bg-opacity-75 text-white text-center text-xs p-1 z-0 ${example === hoveredExample ? 'opacity-100' : 'opacity-0'}`}
+                >
+                  {example.title}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
       <form
         onSubmit={handleSubmit}
