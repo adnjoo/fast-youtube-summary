17f85d90db87bb864a55bfd6aa535ecf74da504b
Andrew Njoo <adnjoo@gmail.com>
2024-06-16 11:05:42 -0700

    unused

--- app/(landing)/utils.ts
+++
@@ -1,6 +0,0 @@
-export const getThumbnail = (videoUrl: string) => {
-  const videoId = new URL(videoUrl).searchParams.get("v");
-  return videoId
-    ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`
-    : "";
-};
