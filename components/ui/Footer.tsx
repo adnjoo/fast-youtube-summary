8a1e6d8640da2e9937b41440af58148d313e7f02
Andrew Njoo <adnjoo@gmail.com>
2024-06-16 10:46:33 -0700

    css

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="footer p-4 text-center text-sm fixed bottom-0 w-full">
+    <footer className="p-4 text-center text-sm w-full hover:underline">
       <Link href="https://github.com/adnjoo/youtube-summarizer">
         built by drew
       </Link>
