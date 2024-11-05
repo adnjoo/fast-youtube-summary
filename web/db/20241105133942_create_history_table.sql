8c9e7baa45e96da5eb6b5af6ed216035bfc71419
Andrew Njoo <adnjoo@gmail.com>
2024-11-05 14:04:19 -0600

    schema

--- web/db/20241105133942_create_history_table.sql
+++ web/db/20241105133942_create_history_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE public.history (
     id SERIAL PRIMARY KEY,
     user_id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
     url TEXT NOT NULL,
+    title TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
