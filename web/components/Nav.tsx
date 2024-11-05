3c32106fb5ea7f0bef599ea360cf79ee613590b4
Andrew Njoo <adnjoo@gmail.com>
2024-11-05 00:52:16 -0600

    link

--- web/components/Nav.tsx
+++ web/components/Nav.tsx
@@ -6,23 +6,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
-import { createClient } from '@/utils/supabase/server';
 import AuthButton from './auth/AuthButton';
 
 export const Nav = async () => {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  console.log(user);
   return (
     <nav className='fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90'>
       <div className='mx-auto w-full max-w-7xl px-4'>
         <div className='flex h-14 items-center justify-between'>
-          <Link href='#' className='flex items-center' prefetch={false}>
+          <Link href='/' className='flex items-center' prefetch={false}>
             <Image
               src='/logo.png'
               alt='GitHub Logo'
@@ -30,7 +21,6 @@ export const Nav = async () => {
               height={30}
               className='mx-auto'
             />
-            <span className='sr-only'>Acme Inc</span>
           </Link>
           <nav className='hidden gap-4 md:flex'>
             {/* <Link
