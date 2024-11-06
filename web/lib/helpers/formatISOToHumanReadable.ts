a43ca50d6e7e293356c5fe92d2972684a21619c8
Andrew Njoo <adnjoo@gmail.com>
2024-11-06 17:25:18 -0600

    less

--- web/lib/helpers/formatISOToHumanReadable.ts
+++ web/lib/helpers/formatISOToHumanReadable.ts
@@ -1,13 +1,3 @@
-/**
- * Converts an ISO date string to a human-readable format.
- * 
- * This function takes an ISO date string and returns a string in the format of 'Month Day, Year, Time' 
- * where 'Month' is the full month name, 'Day' is the day of the month, 'Year' is the full year, 
- * and 'Time' is the time in 12-hour format with AM/PM.
- * 
- * @param {string} isoDate - The ISO date string to be converted.
- * @returns {string} - The human-readable date string.
- */
 export function formatISOToHumanReadable(isoDate: string): string {
   const date = new Date(isoDate);
 
