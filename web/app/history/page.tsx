9b80586481c1a420b47eefa4a0d5194cfca67456
Andrew Njoo <adnjoo@gmail.com>
2024-11-07 16:59:39 -0600

    history pagination (#87)
    * wip
    
    * wip
    
    * F P N L

--- web/app/history/page.tsx
+++ web/app/history/page.tsx
@@ -1,23 +1,34 @@
 'use client';
 
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useMutation, useQuery } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
 
-import HistoryCard from '@/components/HistoryCard';
+import { HistoryCard } from '@/components/HistoryCard';
+import { Button } from '@/components/ui';
 import { useUser } from '@/lib/hooks/useUser';
 import { supabase } from '@/utils/supabase/client';
 
 export default function Page() {
   const user = useUser();
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+  const [totalPages, setTotalPages] = useState(0);
 
   async function fetchHistory() {
-    const { data, error } = await supabase
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize - 1;
+
+    const { data, error, count } = await supabase
       .from('history')
-      .select()
+      .select('*', { count: 'exact' }) // Get total count for pagination
       .eq('user_id', user?.id)
-      .order('created_at', { ascending: false });
+      .order('created_at', { ascending: false })
+      .range(start, end);
 
     if (error) throw new Error(error.message);
+
+    if (count) setTotalPages(Math.ceil(count / pageSize));
     return data;
   }
 
@@ -26,8 +37,9 @@ export default function Page() {
     isLoading,
     refetch,
   } = useQuery({
-    queryKey: ['history'],
+    queryKey: ['history', page],
     queryFn: fetchHistory,
+    placeholderData: keepPreviousData,
   });
 
   const deleteMutation = useMutation<void, Error, number>({
@@ -42,7 +54,7 @@ export default function Page() {
 
   return (
     <div className='container mx-auto max-w-4xl p-4 sm:py-12'>
-      <h2 className='text-xl font-semibold mb-4'>History</h2>
+      <h2 className='mb-4 text-xl font-semibold'>History</h2>
       <section className='flex flex-col gap-4 sm:p-4'>
         {!user ? (
           <p className='text-gray-500'>
@@ -64,6 +76,32 @@ export default function Page() {
           <p className='text-gray-500'>No history available.</p>
         )}
       </section>
+      {totalPages > 1 && (
+        <div className='mt-4 flex justify-center gap-2'>
+          <Button onClick={() => setPage(1)} disabled={page === 1}>
+            First
+          </Button>
+          <Button
+            onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous
+          </Button>
+          <span className='px-4 py-2'>{`Page ${page} of ${totalPages}`}</span>
+          <Button
+            onClick={() => setPage((prev) => Math.min(prev + 1, totalPages))}
+            disabled={page === totalPages}
+          >
+            Next
+          </Button>
+          <Button
+            onClick={() => setPage(totalPages)}
+            disabled={page === totalPages}
+          >
+            Last
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
