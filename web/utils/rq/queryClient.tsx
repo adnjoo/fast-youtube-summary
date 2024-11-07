0cd007cf27817bf25525762456ae27099ba653ef
Andrew Njoo <adnjoo@gmail.com>
2024-11-07 09:12:57 -0600

    rq

---
+++ web/utils/rq/queryClient.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+
+export default function Providers({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
