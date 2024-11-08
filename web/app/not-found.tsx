53e55451e218fb109e949cc68600519c753c2033
Andrew Njoo <adnjoo@gmail.com>
2024-11-07 21:04:59 -0600

    css

--- web/app/not-found.tsx
+++ web/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className='my-container pt-12'>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href='/'>Return Home</Link>
